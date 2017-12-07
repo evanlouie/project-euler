14c35f0271c9b6dd3532fb3987751dc061d7e1ba
Evan Louie <evan.louie@microsoft.com>
2017-12-06 22:41:30 -0800

    build

--- resources/public/js/compiled/out/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [figwheel.client] [figwheel.client.utils] [project-euler.core]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/project-euler.core.on-js-reload (apply js/project-euler.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'project-euler.core/on-js-reload' is missing"))), :open-urls ["http://localhost:3449/index.html"], :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
