688fe1ba1a026a86b98d0ac9bf887c4b3b87014d
Evan Louie <evan.louie@microsoft.com>
2018-11-25 13:08:39 -0800

    update question

--- src/daily_coding_problem/problem_1.clj
+++ src/daily_coding_problem/problem_1.clj
@@ -2,7 +2,9 @@
   (:require [clojure.test :refer [testing is]]))
 
 (defn problem-1
-  "Given a list of numbers and a number `k`, return whether any two numbers from the list add up to `k`.
+  "This problem was recently asked by Google.
+
+  Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
   For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
